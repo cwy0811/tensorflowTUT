d8f691201be47daf3b4ba27ddb6e6f29678b317b
cwy0811 <284679859@qq.com>
2017-04-27 21:26:54 +0800

    Add files via upload

---
+++ bp/connections.py
@@ -0,0 +1,9 @@
+﻿# Connections对象，提供Connection集合操作
+class Connections(object):
+    def __init__(self):
+        self.connections = []
+    def add_connection(self, connection):
+        self.connections.append(connection)
+    def dump(self):
+        for conn in self.connections:
+            print conn
